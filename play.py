a046c7480a63461bbfa8ef90fa338d4791686031
derpen <rizqyassabil@hotmail.com>
2021-09-27 16:26:45 +0700

    Better Framerate

--- play.py
+++ play.py
@@ -3,8 +3,10 @@ import os
 
 amountOfFrames = len(os.listdir('Frames'))
 
+framerate = 1.0/30
+
 for i in range(0, amountOfFrames):
     f = open("Frames/asciiFrame%d.txt" % i, "r")
     contents = f.read()
     print(contents)
-    time.sleep(0.03333) # Framerate
+    time.sleep(framerate) 
